6e4202000eddf678f7f7050b1162b63c1127d99d
Kahlil Enmond <slope22@hotmail.com>
2020-07-30 20:33:04 -0400

    starter code

---
+++ reactApp.js
@@ -0,0 +1,18 @@
+// Create a stateful Team component that accepts a name and a path to a logo for the team as a prop
+
+// class Potato extends React.Component {
+//     render() {
+//         return <h1>My favorite type of potato is, {this.props.type}</h1>;
+//     }
+// }
+
+
+
+class Team1 extends React.Component {
+     
+    render() {
+        return <h2>{this.prop.name1}</h2>
+    }
+        
+        
+}
